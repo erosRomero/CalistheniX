07947c8eeb9caea9a32b2ff35233b8927ca96e73
Eros <erosromero95@gmail.com>
2023-06-21 02:02:35 -0300

    MENU HAMBURGUESA

--- js/estilos.js
+++
@@ -1,8 +0,0 @@
-// Obtener los elementos necesarios
-const navbarToggle = document.querySelector(".navbarToggle");
-const navbarNavegacion = document.querySelector(".navbarNavegacion");
-
-// Agregar evento click al icono de hamburguesa
-navbarToggle.addEventListener('click', function() {
-  navbarNavegacion.classList.toggle('mostrar');
-});
